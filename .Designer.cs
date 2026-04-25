08ae6f51170d868c48edc29903589cabc6c9151b
yoyo <yoyo24082006@gmail.com>
2026-04-25 22:42:50 +0800

    Enhance triangle application UI and functionality. Added input validation, improved layout, and implemented triangle drawing feature. Updated README to credit original triangle logic author.

--- .Designer.cs
+++ .Designer.cs
@@ -36,88 +36,117 @@
             this.txtA = new System.Windows.Forms.TextBox();
             this.txtB = new System.Windows.Forms.TextBox();
             this.txtC = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(52, 56);
+            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(26, 28);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(124, 32);
+            this.label1.Size = new System.Drawing.Size(64, 25);
             this.label1.TabIndex = 0;
-            this.label1.Text = "length a";
+            this.label1.Text = "Side A";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(52, 134);
+            this.label2.Font = new System.Drawing.Font("Segoe UI Semibold", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(26, 86);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(124, 32);
+            this.label2.Size = new System.Drawing.Size(64, 25);
             this.label2.TabIndex = 1;
-            this.label2.Text = "length b";
+            this.label2.Text = "Side B";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(52, 208);
+            this.label3.Font = new System.Drawing.Font("Segoe UI Semibold", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(26, 144);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(122, 32);
+            this.label3.Size = new System.Drawing.Size(63, 25);
             this.label3.TabIndex = 2;
-            this.label3.Text = "length c";
+            this.label3.Text = "Side C";
             // 
             // btnShow
             // 
-            this.btnShow.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnShow.Location = new System.Drawing.Point(58, 282);
+            this.btnShow.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(24)))), ((int)(((byte)(27)))));
+            this.btnShow.FlatAppearance.BorderSize = 0;
+            this.btnShow.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(39)))), ((int)(((byte)(42)))));
+            this.btnShow.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(63)))), ((int)(((byte)(63)))), ((int)(((byte)(70)))));
+            this.btnShow.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnShow.Font = new System.Drawing.Font("Segoe UI Semibold", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnShow.ForeColor = System.Drawing.Color.White;
+            this.btnShow.Location = new System.Drawing.Point(31, 204);
             this.btnShow.Name = "btnShow";
-            this.btnShow.Size = new System.Drawing.Size(126, 44);
+            this.btnShow.Size = new System.Drawing.Size(321, 42);
             this.btnShow.TabIndex = 3;
-            this.btnShow.Text = "Show";
-            this.btnShow.UseVisualStyleBackColor = true;
+            this.btnShow.Text = "Show Triangle Type";
+            this.btnShow.UseVisualStyleBackColor = false;
             this.btnShow.Click += new System.EventHandler(this.btnShow_Click);
             // 
             // lblResult
             // 
-            this.lblResult.AutoSize = true;
-            this.lblResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblResult.Location = new System.Drawing.Point(52, 369);
+            this.lblResult.AutoSize = false;
+            this.lblResult.BackColor = System.Drawing.Color.White;
+            this.lblResult.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblResult.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResult.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(41)))), ((int)(((byte)(55)))));
+            this.lblResult.Location = new System.Drawing.Point(31, 264);
             this.lblResult.Name = "lblResult";
-            this.lblResult.Size = new System.Drawing.Size(101, 32);
+            this.lblResult.Padding = new System.Windows.Forms.Padding(12, 10, 12, 10);
+            this.lblResult.Size = new System.Drawing.Size(321, 82);
             this.lblResult.TabIndex = 4;
-            this.lblResult.Text = "Result";
+            this.lblResult.Text = "Result will appear here";
+            this.lblResult.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // txtA
             // 
-            this.txtA.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtA.Location = new System.Drawing.Point(252, 54);
+            this.txtA.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtA.Location = new System.Drawing.Point(132, 27);
             this.txtA.Name = "txtA";
-            this.txtA.Size = new System.Drawing.Size(100, 34);
+            this.txtA.Size = new System.Drawing.Size(220, 30);
             this.txtA.TabIndex = 5;
             // 
             // txtB
             // 
-            this.txtB.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtB.Location = new System.Drawing.Point(252, 134);
+            this.txtB.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtB.Location = new System.Drawing.Point(132, 85);
             this.txtB.Name = "txtB";
-            this.txtB.Size = new System.Drawing.Size(100, 34);
+            this.txtB.Size = new System.Drawing.Size(220, 30);
             this.txtB.TabIndex = 6;
             // 
             // txtC
             // 
-            this.txtC.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtC.Location = new System.Drawing.Point(252, 208);
+            this.txtC.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtC.Location = new System.Drawing.Point(132, 143);
             this.txtC.Name = "txtC";
-            this.txtC.Size = new System.Drawing.Size(100, 34);
+            this.txtC.Size = new System.Drawing.Size(220, 30);
             this.txtC.TabIndex = 7;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(378, 27);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(390, 390);
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.txtC);
             this.Controls.Add(this.txtB);
             this.Controls.Add(this.txtA);
@@ -128,6 +157,7 @@
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Types of Triangles";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -143,6 +173,7 @@
         private System.Windows.Forms.TextBox txtA;
         private System.Windows.Forms.TextBox txtB;
         private System.Windows.Forms.TextBox txtC;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -12,27 +12,153 @@ namespace TypesOfTriangles
 {
     public partial class Form1 : Form
     {
+        // Controls whether the triangle should be drawn in the preview box.
+        private bool drawTriangle = false;
+        // Stores the last valid side lengths entered by the user.
+        private double triangleA = 0;
+        private double triangleB = 0;
+        private double triangleC = 0;
+
+        // Initializes the form and wires up UI events.
         public Form1()
         {
             InitializeComponent();
+            pictureBox1.Paint += new PaintEventHandler(pictureBox1_Paint);
+            txtA.KeyPress += NumericTextBox_KeyPress;
+            txtB.KeyPress += NumericTextBox_KeyPress;
+            txtC.KeyPress += NumericTextBox_KeyPress;
         }
 
+        // Validates input, determines triangle type, and triggers redraw.
+        // Special thanks to Lin Htut Khine for writing the original triangle-type logic
+        // (valid triangle check + Equilateral/Isosceles/Scalene classification) used here.
         private void btnShow_Click(object sender, EventArgs e)
         {
-            if (!int.TryParse(txtA.Text, out int a) || !int.TryParse(txtB.Text, out int b) || !int.TryParse(txtC.Text, out int c))
+            drawTriangle = false;
+            pictureBox1.Invalidate();
+
+            if (!double.TryParse(txtA.Text, out double a) || !double.TryParse(txtB.Text, out double b) || !double.TryParse(txtC.Text, out double c))
             {
-                lblResult.Text = "Enter whole numbers for a, b, and c";
+                lblResult.Text = "Please enter valid numbers for Side A, Side B, and Side C.";
+                return;
+            }
+
+            if (a <= 0 || b <= 0 || c <= 0)
+            {
+                lblResult.Text = "All sides must be greater than 0.";
                 return;
             }
 
             if (a + b <= c || b + c <= a || c + a <= b)
-                lblResult.Text = "Not a valid triangle";
-            else if (a == b && b == c)
-                lblResult.Text = "Equilateral";
-            else if (a == b || b == c || a == c)
-                lblResult.Text = "Isosceles";
+            {
+                lblResult.Text = "Not a valid triangle (sum of any 2 sides must be greater than the 3rd).";
+            }
             else
-                lblResult.Text = "Scalene";
+            {
+                if (a == b && b == c)
+                    lblResult.Text = "Type: Equilateral triangle";
+                else if (a == b || b == c || a == c)
+                    lblResult.Text = "Type: Isosceles triangle";
+                else
+                    lblResult.Text = "Type: Scalene triangle";
+
+                drawTriangle = true;
+                triangleA = a;
+                triangleB = b;
+                triangleC = c;
+                pictureBox1.Invalidate();
+            }
+        }
+
+        // Allows only numeric input (digits and one decimal point) in side text boxes.
+        private void NumericTextBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (char.IsControl(e.KeyChar))
+            {
+                return;
+            }
+
+            TextBox textBox = sender as TextBox;
+            if (textBox == null)
+            {
+                return;
+            }
+
+            if (!char.IsDigit(e.KeyChar) && e.KeyChar != '.')
+            {
+                e.Handled = true;
+                return;
+            }
+
+            if (e.KeyChar == '.' && textBox.Text.Contains("."))
+            {
+                e.Handled = true;
+            }
+        }
+
+        // Draws a scaled triangle in the picture box using the last valid side lengths.
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            if (drawTriangle)
+            {
+                var g = e.Graphics;
+                g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+
+                double a = triangleA;
+                double b = triangleB;
+                double c = triangleC;
+
+                double xC = (b * b + c * c - a * a) / (2.0 * c);
+                double yC2 = (b * b) - (xC * xC);
+
+                if (yC2 <= 0)
+                {
+                    return;
+                }
+
+                double yC = Math.Sqrt(yC2);
+
+                var pA = new PointF(0f, 0f);
+                var pB = new PointF((float)c, 0f);
+                var pC = new PointF((float)xC, (float)yC);
+
+                float minX = Math.Min(pA.X, Math.Min(pB.X, pC.X));
+                float maxX = Math.Max(pA.X, Math.Max(pB.X, pC.X));
+                float minY = Math.Min(pA.Y, Math.Min(pB.Y, pC.Y));
+                float maxY = Math.Max(pA.Y, Math.Max(pB.Y, pC.Y));
+
+                float modelWidth = maxX - minX;
+                float modelHeight = maxY - minY;
+                float margin = 20f;
+
+                float availableWidth = pictureBox1.ClientSize.Width - (margin * 2f);
+                float availableHeight = pictureBox1.ClientSize.Height - (margin * 2f);
+
+                if (modelWidth <= 0 || modelHeight <= 0 || availableWidth <= 0 || availableHeight <= 0)
+                {
+                    return;
+                }
+
+                float scale = Math.Min(availableWidth / modelWidth, availableHeight / modelHeight);
+
+                Func<PointF, PointF> toScreen = p =>
+                {
+                    float x = (p.X - minX) * scale + margin;
+                    float y = pictureBox1.ClientSize.Height - (((p.Y - minY) * scale) + margin);
+                    return new PointF(x, y);
+                };
+
+                PointF sA = toScreen(pA);
+                PointF sB = toScreen(pB);
+                PointF sC = toScreen(pC);
+
+                using (var fillBrush = new SolidBrush(Color.FromArgb(60, 100, 149, 237)))
+                using (var pen = new Pen(Color.RoyalBlue, 2f))
+                {
+                    g.FillPolygon(fillBrush, new[] { sA, sB, sC });
+                    g.DrawPolygon(pen, new[] { sA, sB, sC });
+                }
+            }
         }
     }
 }
